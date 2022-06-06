fbef1b2e593ac59b4294142ebdc5ff8662804589
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-06-06 13:10:52 -0700

    practice assignment

---
+++ first_java_program/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("My name is Coding Dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Burbank, CA");
+
+	}
+}
+// My name is Coding Dojo
+// I am 100 years old
+// My hometown is Burbank, CA
