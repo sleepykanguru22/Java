7ec260ad116bd48252871a2a90680056b5467058
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-06-06 14:30:11 -0700

    alfredbotsolution

---
+++ FirstJavaProgram/Test.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My Name is Coding Dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Burbank, CA");
+    }
+}
