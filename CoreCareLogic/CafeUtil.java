4a22a3bb8142ab0d97fad5074fffebe2b5af1396
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-06-07 13:22:27 -0700

    Update CafeUtil.java

--- CoreCareLogic/CafeUtil.java
+++ CoreCareLogic/CafeUtil.java
@@ -3,14 +3,22 @@ public class CafeUtil {
     public int getStreakGoal(){
         int numWeeks = new int[10];
 
-        for (int i = 0; i + 1 <=9; i++)
+        for (int i = 0; i <=numWeeks.length; i++)
         {
         numWeeks[i] = i + 1;
         }
         return numWeeks[i];
     }
 
-   
+   public double getOrderTotal(double[] prices){
+       double prices[] = {5.99, 7.89, 10.50, 11.99, 14.99};
+       int i;
+       for (i = 0; i <=prices.length; i++){
+           total += i
+       }
+       return total;
+   }
+
 }
 
   
