51bcea166f55661e986531fe22462c86e2bbad84
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-06-08 21:28:42 -0700

    updated bat class

--- ZooKeeper/src/com/jasmincarter/zookeeper/Bat.java
+++ ZooKeeper/src/com/jasmincarter/zookeeper/Bat.java
@@ -1,19 +1,24 @@
 package com.jasmincarter.zookeeper;
 
-public class Bat {
-	energyLevel = 300;
+class Bat extends Mammal {
+	public Bat{
+		this.energyLevel = 300;
+		super.displayEnergy();
+	}
+	public void fly() {
+		energyLevel -=50;
+		System.out.println("Swoosh swoosh" + displayEnergy());
+	}
 
+	public void eatHumans() {
+		energyLevel +=25;
+		System.out.println("ahhh! the bat is eating people" + displayEnergy());
+		
+	}
+	public void attackTowns() {
+		energyLevel -=100;
+		System.out.println("ahhh! the town is burning" + displayEnergy());
+	}
 }
 
-public fly() {
-	energyLevel -50;
-	System.out.println("Swoosh swoosh");
-}
 
-public eatHumans() {
-	energyLevel +25;
-}
-public attackTowns() {
-	energyLevel -100;
-	System.out.println("ahhh! the town is burning");
-}
