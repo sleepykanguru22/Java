23548a55afb4807739867b12db47f7fe7d4db0c8
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-06-08 20:04:49 -0700

    created bat class in zookeeper

---
+++ ZooKeeper/src/com/jasmincarter/zookeeper/Bat.java
@@ -0,0 +1,19 @@
+package com.jasmincarter.zookeeper;
+
+public class Bat {
+	energyLevel = 300;
+
+}
+
+public fly() {
+	energyLevel -50;
+	System.out.println("Swoosh swoosh");
+}
+
+public eatHumans() {
+	energyLevel +25;
+}
+public attackTowns() {
+	energyLevel -100;
+	System.out.println("ahhh! the town is burning");
+}
--- ZooKeeper/src/com/jasmincarter/zookeeper/Gorilla.java
+++ ZooKeeper/src/com/jasmincarter/zookeeper/Gorilla.java
@@ -1,9 +1,25 @@
 package com.jasmincarter.zookeeper;
 
+import java.util.Set;
+
 public class Gorilla {
 
 	public Gorilla() {
+		super();
 		// TODO Auto-generated constructor stub
 	}
-
+	public throwSomething(){
+		set.energyLevel - 5;
+		System.out.println("The Gorilla has thrown something");
+	}
+	public eatBananas() {
+		Set.energyLevel + 10;
+		System.out.println("The Gorilla is happy");
+	}
+	public climb() {
+		set.energyLevel - 10;
+		System.out.println("The Gorilla climbed a tree");
+	}
+	
+	
 }
