d809beb7908378f9a4d463f214e89218d0a34378
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-06-09 13:21:09 -0700

    puzzle practice

---
+++ Puzzle/Test.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Random;
+public class Test {
+    
+	public static void main(String[] args) {
+		PuzzleJava generator = new PuzzleJava();
+		ArrayList<Integer> randomRolls = generator.getTenRolls();
+		System.out.println(randomRolls);
+		
+    	
+	}
+}
