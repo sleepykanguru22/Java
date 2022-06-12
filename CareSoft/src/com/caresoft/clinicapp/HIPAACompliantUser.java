55d8965f9a1f59031d3590f808de3396ccdb5ab9
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-06-12 13:55:41 -0700

    moved files

--- CareSoft/src/com/caresoft/clinicapp/HIPAACompliantUser.java
+++
@@ -1,9 +0,0 @@
-package com.caresoft.clinicapp;
-
-public interface HIPAACompliantUser {
-	public interface HIPAACompliantUser {
-	    abstract boolean assignPin(int pin);
-	    abstract boolean accessAuthorized(Integer confirmedAuthID);
-	}
-
-}
