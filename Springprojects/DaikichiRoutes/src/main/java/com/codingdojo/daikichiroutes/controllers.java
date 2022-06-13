5a3d4baefa838dcb4495eaf39dab7e1fc82e80c3
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-06-12 18:09:26 -0700

    Update controllers.java

--- Springprojects/DaikichiRoutes/src/main/java/com/codingdojo/daikichiroutes/controllers.java
+++ Springprojects/DaikichiRoutes/src/main/java/com/codingdojo/daikichiroutes/controllers.java
@@ -1,5 +1,6 @@
 package com.codingdojo.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,23 @@ public class controllers {
 		return "Today you will find luck in all your endeavors!";
 	}
 	
-	
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas";
 	}
-}
+	 @RequestMapping("/travel/{city}")
+	    public String showCity(@PathVariable("city") String city){
+	    	return "Congratulations! You will soon travel to " + city + "!";
+	    }
+	 @RequestMapping("/lotto/{num}")
+	    public String showLotto(@PathVariable("num") Integer num){
+		 if (num%2==0) {
+			 return "You will take a grand journey in the near future, but be weary of tempting offers";
+		 }
+		 else {
+	    	return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+	    }
+		 
+	 }
+		 
+}  
