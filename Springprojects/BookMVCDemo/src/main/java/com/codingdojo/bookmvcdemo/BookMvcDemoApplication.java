3c90aa1b576163976e58be08051277023e98f3c1
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-06-15 18:21:44 -0700

    book demo

---
+++ Springprojects/BookMVCDemo/src/main/java/com/codingdojo/bookmvcdemo/BookMvcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.bookmvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMvcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMvcDemoApplication.class, args);
+	}
+
+}
